37ad127a1b04d52bada2337f9e27de0793a8ea70
Alexander Schelchere <ascellie@gmail.com>
2019-07-29 18:59:33 -0400

    app(serializers): athlete, friend

--- app/serializers/athlete_serializer.rb
+++ app/serializers/athlete_serializer.rb
@@ -1,4 +1,6 @@
 class AthleteSerializer
   include FastJsonapi::ObjectSerializer
-  attributes 
+  attributes :name, :username, :email, :password_digest
+  has_many :frienders
+  has_many :friendees
 end
