ba29bde86c63e6b4fe62f03e3b84a34e63aa8e52
Alexander Schelchere <ascellie@gmail.com>
2019-07-29 15:21:46 -0400

    models: self-referential athletes(users)

--- app/models/athlete.rb
+++ app/models/athlete.rb
@@ -2,4 +2,10 @@ class Athlete < ApplicationRecord
     has_many :routines
     has_many :favorites
     has_many :routines, through: :favorites
+
+    has_many :friended_users, foreign_key: :friender_id, class_name: 'Friend'
+    has_many :friendees, through: :friended_users
+
+    has_many :friending_users, foreign_key: :friendee_id, class_name: 'Friend'
+    has_many :frienders, through: :friending_users
 end
