7c95edde0d3508a76cca826f32f88b0abb21db18
Alexander Schelchere <ascellie@gmail.com>
2019-07-29 16:05:12 -0400

    db: self referential relationship for athletes

--- app/models/athlete.rb
+++ app/models/athlete.rb
@@ -3,9 +3,9 @@ class Athlete < ApplicationRecord
     has_many :favorites
     has_many :routines, through: :favorites
 
-    has_many :friended_users, foreign_key: :friender_id, class_name: 'Friend'
-    has_many :friendees, through: :friended_users
+    has_many :friended_athletes, foreign_key: :friend_1, class_name: 'Friend'
+    has_many :friendees, through: :friended_athletes
 
-    has_many :friending_users, foreign_key: :friendee_id, class_name: 'Friend'
-    has_many :frienders, through: :friending_users
+    has_many :friending_athletes, foreign_key: :friend_2, class_name: 'Friend'
+    has_many :frienders, through: :friending_athletes
 end
