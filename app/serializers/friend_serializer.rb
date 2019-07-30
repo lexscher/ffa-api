14cd3dd1d3f709a7c72c5ed3beb6ecea555e35cd
Alexander Schelchere <ascellie@gmail.com>
2019-07-30 11:07:57 -0400

    app(serializers): friends contain friendee & friender information

--- app/serializers/friend_serializer.rb
+++ app/serializers/friend_serializer.rb
@@ -2,4 +2,11 @@ class FriendSerializer
   include FastJsonapi::ObjectSerializer
   attributes :friender_id, :friendee_id
 
+  attribute :friendship do |friend|
+    {"friendship" => [
+      "friender" => friend.friender,
+      "friendee" => friend.friendee
+      ]
+    }
+  end
 end
