b47dec5a41e5a52a4da0fb7c45f78f5e3cd7e0fb
Alexander Schelchere <ascellie@gmail.com>
2019-07-30 09:45:12 -0400

    app(controllers): begin making friends and athletes

--- app/controllers/athletes_controller.rb
+++ app/controllers/athletes_controller.rb
@@ -1,2 +1,25 @@
 class AthletesController < ApplicationController
+
+  def index
+    athletes = Athlete.all
+    render json: AthleteSerializer.new(athletes)
+  end
+
+  def show
+    find_athlete
+    find_friends
+
+    render json: AthleteSerializer.new(@athlete)
+  end
+
+  private
+
+  def find_athlete 
+    @athlete = Athlete.find(params[:id])
+  end
+
+  def find_friends
+    find_athlete
+    @friends = @athlete.friends
+  end
 end
--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,2 +1,21 @@
 class FriendsController < ApplicationController
+
+  def index
+    @friends = Friend.all
+    render json: FriendSerializer.new(@friends)
+  end
+
+  def show
+    friend = Friend.find(params[:id])
+
+    render json: FriendSerializer.new(friend)
+  end
+
+  private
+
+  def find_friend 
+    @friend = Friend.find(params[:id])
+  end
+
+
 end
