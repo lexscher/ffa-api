1b20e8cdac8ff711c6cf92b43b9b1031e94795c4
Alexander Schelchere <ascellie@gmail.com>
2019-07-30 11:07:19 -0400

    app(serializers): understand serializer - JSON formatting successful

--- app/serializers/athlete_serializer.rb
+++ app/serializers/athlete_serializer.rb
@@ -1,6 +1,11 @@
 class AthleteSerializer
   include FastJsonapi::ObjectSerializer
   attributes :name, :username, :email, :password_digest
-  has_many :frienders
-  has_many :friendees
+
+  attribute :friends do |athlete| 
+    friends_array = []
+    friends_array << athlete.frienders
+    friends_array << athlete.friendees
+    friends_array.flatten
+  end
 end
