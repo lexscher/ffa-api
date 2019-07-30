c090b911c46538a58c3b896d6100332508560316
Alexander Schelchere <ascellie@gmail.com>
2019-07-30 09:39:45 -0400

    config(routes): change athletes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   resources :exercises
   resources :workouts
   resources :routines
-  resources :athletes
+  resources :athletes, except: :update
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
